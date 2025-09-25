7a966cf029c7f9e669420445f73505ce4abbb87e
simonkubis <simon.kubis20@gmail.com>
2025-09-25 21:39:43 +0200

    connected backend with frontend example

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,13 +1,29 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div>
+      <q-btn label="Get hello from backend" color="primary" @click="getHello" />
+      <div v-if="helloMessage" class="q-mt-md text-h6">
+        {{ helloMessage }}
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+import axios from 'axios'
+
+const helloMessage = ref('')
+
+const api = import.meta.env.API_URL || 'http://localhost:3333'
+
+const getHello = async () => {
+  try {
+    const res = await axios.get(`${api}/hello`)
+    helloMessage.value = res.data.message
+  } catch (err) {
+    helloMessage.value = 'Error connecting to API'
+    console.error(err)
+  }
+}
 </script>
